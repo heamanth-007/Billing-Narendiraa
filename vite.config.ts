40316db0e114ca59a37f19bf0a5167164aab0c97
heamanth-007 <heamanthprabhu59@gmail.com>
2026-09-10 16:16:08 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+  },
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 1600,
